4b21ed0aab4f9b40870618082d4678cc5f6b7952
amh <abuubakarm110@gmail.com>
2022-07-01 19:33:53 -0700

    printing number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,10 @@ int last_digit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last_digit = n % 10; 
-/* your code goes there */
 
+last_digit = (n % 10); 
+
+/* your code goes there */
 if (last_digit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ **main - Entry point
+ ** Return: Always 1 (success)
+ **/
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+	printf("%d", a);
+}
+
+return (0);
+
+}
