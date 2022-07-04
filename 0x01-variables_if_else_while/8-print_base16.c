54dd91b1bd263f7eccb985981e6c59e2d243e14c
amh <abuubakarm110@gmail.com>
2022-07-03 20:33:17 -0700

    hex numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ **main - Entry point
+ ** Return: Always 1 (success)
+ **/
+int main(void)
+{
+char c = 'a';
+
+int n;
+for (n = 0; n < 16; n++)
+{
+	if (n < 10)
+	{
+	putchar(n + '0');
+	}
+	else
+	{																	putchar(c);
+	c++;
+	}
+																	}
+	putchar('\n');
+	return (0);
+}
