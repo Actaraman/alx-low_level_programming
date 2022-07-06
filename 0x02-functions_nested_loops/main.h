e29243b83294d7b9a589591ea98b02cae280bdb9
amh <abuubakarm110@gmail.com>
2022-07-06 12:55:08 -0700

    edit ot header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-int printing(void);
+void printing(void);
 
 #endif
--- 0x02-functions_nested_loops/printputchar.c
+++ 0x02-functions_nested_loops/printputchar.c
@@ -4,7 +4,7 @@
 ** printing - Entry point
 ** Return: Always 0 (success)
 **/
-int printing(void)
+void printing(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -14,7 +14,5 @@ int printing(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	_putchar('\n);
 }
