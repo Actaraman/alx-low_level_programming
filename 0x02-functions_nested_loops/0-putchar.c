68cde82c694db7a73b53afd5fcfbd9ce3eed0f12
amh <abuubakarm110@gmail.com>
2022-07-06 20:20:58 -0700

    final test on print file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,18 @@
  **/
 int main(void)
 {
-void prnting(void);
+void prnting(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');	
+}
+printing();
 return (0);
 }
