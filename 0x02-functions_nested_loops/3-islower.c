5c7f08c4bc3ae1864a732210b96b9d74115cf3a3
amh <abuubakarm110@gmail.com>
2022-07-09 23:34:26 -0700

    testing lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+** _islower - main entry
+** @c: paramater for entry
+** Return: Always nothing (success)
+**/
+int _islower(int c)
+{
+	if (islower(c + '0'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
