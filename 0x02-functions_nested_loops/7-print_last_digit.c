0935265add8ab3e7aa3be758163cb08b6a1a089a
amh <abuubakarm110@gmail.com>
2022-07-10 00:47:33 -0700

    last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+** print_last_digit - main entry
+** @c: paramater for entry
+** Return: Always nothing (success)
+**/
+int print_last_digit(int c)
+{
+	int last_digit;
+
+	last_digit = c % 10;
+	return (last_digit);
+}
