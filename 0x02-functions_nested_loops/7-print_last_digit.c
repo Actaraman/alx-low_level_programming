1f62c927421300c56326ac1d37be99fe2c977a59
amh <abuubakarm110@gmail.com>
2022-07-10 01:01:52 -0700

    quick fix to file 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,14 @@ int print_last_digit(int c)
 {
 	int last_digit;
 
+	if (c < 0)
+	{
+		last_digit = -1 * (c % 10);
+	}
+	else
+	{
 	last_digit = (c % 10);
+	}
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
