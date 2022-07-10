c5870f675909ad4a5466c67ce19860a9691a5725
amh <abuubakarm110@gmail.com>
2022-07-10 02:03:06 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+** times_table - main entry
+** Return: Always nothing (success)
+**/
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1; b <= 9; b++)
+		{
+			if ((a * b) < 9)
+			{
+				_putchar((a * b) + '0');
+				_putchar(',');
+				_putchar('  ');
+			}
+			else
+			{
+				_putchar((a * b) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
